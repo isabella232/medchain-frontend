47c578aa52011a165fa4f69e233dc6c2bcb1c0cc
Jean Chambras <jean.chambras@epfl.ch>
2021-06-13 15:26:51 +0200

    Add comments to helper methods

--- src/tools/format.ts
+++ src/tools/format.ts
@@ -1,5 +1,15 @@
+/**
+ * Method used to display a smaller formatted version of a hash
+ * @param hash The hash value
+ * @returns The formatted hash value
+ */
 export const formatHash = (hash: string) =>
   `${hash.substr(0, 8 + 2)} ... ${hash.substr(-8)}`;
 
+/**
+ * Method used to display a smaller formatted version of the account name
+ * @param hash The account name
+ * @returns The formatted account name
+ */
 export const formatAccountName = (accountName: string) =>
   `${accountName.substr(0, 8 + 2)}...${accountName.substr(-8)}`;
--- src/tools/helpers.ts
+++ src/tools/helpers.ts
@@ -1,5 +1,10 @@
+/**
+ * Method used to remove an element from an array
+ * @param arr The array
+ * @param value The value to remove
+ * @returns The new array
+ */
 export const arrayRemove = (arr:any[], value:any): any[] => { 
-    
     return arr.filter(function(ele){ 
         return ele != value; 
     });
